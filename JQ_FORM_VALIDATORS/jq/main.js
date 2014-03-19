e3d100311917870c4d4a691bbd72ecb1ed268f90
0sv <arenedo@gmail.com>
2014-03-19 16:36:06 +0100

    Validador formularios con JQuery

--- JQ_FORM_VALIDATORS/jq/main.js
+++ JQ_FORM_VALIDATORS/jq/main.js
@@ -1,9 +1,49 @@
 $(document).ready(function() {
 
-
-    //var $f = $('form').validador();
-    $(document).on('keyup', '[data-validator]', function() {
-        $('form').validador();
+    $('form').validador({
+        'required': {
+            'emsg': 'campo requerido',
+            'clases': 'error-requerido'
+        },
+        'textarea': {
+            'emsg': "longitud máxima excedida",
+            'clases': 'error-textarea'
+        },
+        'password': {
+            'emsg': "cotraseña demasiado debil",
+            'clases': 'error-password'
+        },
+        'email': {
+            'emsg': "email erroneo",
+            'clases': 'error-email'
+        }
     });
+
+    // //var $f = $('form').validador();
+    // $(document).on('keyup', '[data-validator]', function() {
+
+    // });
+
+    // $(document).on('submit', '[type="submit"]', function() {
+    //     $('form').validador({
+    //         'required': {
+    //             'emsg': 'campo requerido',
+    //             'clases': 'error-requerido'
+    //         },
+    //         'textarea': {
+    //             'emsg': "longitud máxima excedida",
+    //             'clases': 'error-textarea'
+    //         },
+    //         'password': {
+    //             'emsg': "cotraseña demasiado debil",
+    //             'clases': 'error-password'
+    //         },
+    //         'email': {
+    //             'emsg': "email erroneo",
+    //             'clases': 'error-email'
+    //         }
+    //     });
+    // });
+
 
 });
--- JQ_FORM_VALIDATORS/jq/validator.js
+++ JQ_FORM_VALIDATORS/jq/validator.js
@@ -1,96 +1,184 @@
-var APP = APP || {};
-APP.validador = function() {
-    "use strict";
-
-    var a = function(a) {
-        return void 0 !== a && null !== a && a.length > 0 && !/^([^A-Za-z]+)(\s?)(1)$/.test(a) //!/^\s+/.test(a)
-    }, b = function(a) {
-            var b = /^(\w+)((\.|-|_)(\w+))*@(\w+)(\.\w{2,})+$/;
-            return b.test(a)
-        }, c = function(a) {
-            var b = /[a-z]/,
-                c = /[A-Z]/,
-                d = /[0-9]/;
-            return b.test(a) && c.test(a) && d.test(a) && a.length > 5 && this.required(a)
-        }, d = function(a, b) {
-            return a.length < b
-        };
-    return {
-        required: a,
-        email: b,
-        password: c,
-        min: d
-    }
-}();
-
-
-
-
-
 (function($) {
-    $.fn.validador = function() {
+
+    $.fn.validador = function(opts) {
+        // console.log(opts.required);
         return this.filter('form').each(function() {
-            var $this = $(this);
+            var $form = $(this);
 
+            $form.on('submit', validarFormulario);
+            $form.find(':input[data-validator]').on('keyup checkMe blur', opts, validarCampo);
+        });
+    };
 
-            var $required = $this.find('[data-validator]');
+    var validarCampo = function(e) {
 
+        var f = this.dataset.validator;
 
-            $required.each(function() {
-                var f = $(this).data('validator');
-                console.log("VALOR: " + this.value.length);
-                switch (f) {
-                    case 'required':
-                        if (!APP.validador.required(this.value) && !this.value.length == 0) {
-                            $e = $(this);
-                            $e.addClass('red-bg');
+        switch (f) {
+            case 'required':
 
-                        } else {
-                            $e = $(this);
-                            $e.removeClass('red-bg');
-                        }
-                        break;
-                    case 'email':
-                        if (!APP.validador.email(this.value) && !this.value.length == 0) {
-                            $e = $(this);
-                            $e.addClass('red-bg');
-                        } else {
-                            $e = $(this);
-                            $e.removeClass('red-bg');
-                        }
-                        break;
-                    case 'password':
-                        if (!APP.validador.password(this.value) && !this.value.length == 0) {
-                            $e = $(this);
-                            $e.addClass('red-bg');
-                        } else {
-                            $e = $(this);
-                            $e.removeClass('red-bg');
-                        }
-                        break;
-                    case 'min':
-                        if (APP.validador.min(this.value, $(this).data('length'))) {
-                            $e = $(this);
-                            $e.removeClass('red-bg');
-                        } else {
-                            $e = $(this);
-                            $e.addClass('red-bg');
-                        }
-                        break;
+                if (this.type == "checkbox") {
 
+                    if (!this.checked) {
+                        this.labels[0].classList.add("red-txt");
+                        this.classList.add('red-bg');
+
+                        createErrorAdvice(this, e.data.required);
+                    } else {
+                        this.labels[0].classList.remove("red-txt");
+                        this.classList.remove('red-bg');
+                        borraAvisos(this, e.data.required);
+                    }
+                    break;
+                }
+                if (!validador.required(this.value)) {
+                    $this = $(this);
+                    $this.addClass('red-bg');
+                    createErrorAdvice(this, e.data.required);
+
+                } else {
+                    $this = $(this);
+                    $this.removeClass('red-bg');
+                    borraAvisos(this, e.data.required);
+                }
+                break;
+            case 'email':
+                if (!validador.email(this.value)) {
+                    $this = $(this);
+                    $this.addClass('red-bg');
+                    createErrorAdvice(this, e.data.email);
+
+                } else {
+                    $this = $(this);
+                    $this.removeClass('red-bg');
+                    borraAvisos(this, e.data.email);
+                }
+                break;
+            case 'password':
+                if (!validador.password(this.value)) {
+                    $this = $(this);
+                    $this.addClass('red-bg');
+                    createErrorAdvice(this, e.data.password);
+
+                } else {
+                    $this = $(this);
+                    $this.removeClass('red-bg');
+                    borraAvisos(this, e.data.password);
+                }
+                break;
+            case 'min':
+                if (validador.min(this.value, $(this).data('length'))) {
+                    $this = $(this);
+                    $this.removeClass('red-bg');
+                    borraAvisos(this, e.data.textarea);
+                } else {
+                    $this = $(this);
+                    $this.addClass('red-bg');
+                    createErrorAdvice(this, e.data.textarea);
 
                 }
-            });
+                break;
+
+
+        }
+    };
+
+    var validador = (function() {
+
+        var a = function(a) {
+            return void 0 !== a && null !== a && a.length > 0 && !/^\s+/.test(a)
+        }, b = function(a) {
+                var b = /^(\w+)((\.|-|_)(\w+))*@(\w+)(\.\w{2,})+$/;
+                return b.test(a)
+            }, c = function(a) {
+                var b = /[a-z]/,
+                    c = /[A-Z]/,
+                    d = /[0-9]/;
+                return b.test(a) && c.test(a) && d.test(a) && a.length > 5 && this.required(a)
+            }, d = function(a, b) {
+                return a.length < b
+            };
+        return {
+            required: a,
+            email: b,
+            password: c,
+            min: d
+        }
+    })();
+
+    var createErrorAdvice = function(obj, optsType) {
+
+        borraAvisos(obj, optsType);
+
+
+        var item = document.createElement("div");
+        var arrow = document.createElement("div");
+        var texto = document.createTextNode(optsType.emsg);
+        item.appendChild(texto);
+        // obj.appendChild(item);
+
+        // someElement.parentNode.insertBefore(newElement, someElement.nextSibling);
+
+        obj.parentNode.insertBefore(item, obj.nextSibling);
+
+
+        item.classList.add(optsType.clases + '-' + obj.name);
+        item.classList.add("red-bg-50");
+
+        item.classList.add("pabs");
 
 
 
+        arrow.classList.add(optsType.clases + '-' + obj.name);
+        arrow.classList.add("pabs");
+        arrow.classList.add("frigth");
 
-        });
+        if (optsType.clases == "error-textarea") {
+            item.classList.add("avisoup");
+            arrow.classList.add("arrowup");
+        } else if (optsType.clases + '-' + obj.name == "error-requerido-condiciones")
+
+        {
+            item.classList.add("avisoupleft");
+            arrow.classList.add("arrowupleft");
+        } else
+
+
+        {
+            item.classList.add("aviso");
+            arrow.classList.add("arrowdown");
+        }
+
+        obj.parentNode.insertBefore(arrow, obj.nextSibling);
 
     }
 
+    var borraAvisos = function(obj, optsType) {
+        var listaErrores = $('.' + optsType.clases + '-' + obj.name);
 
 
 
+        if (listaErrores.length > 0) {
+            for (var i = listaErrores.length - 1; i >= 0; i--) {
+                listaErrores[i].parentNode.removeChild(listaErrores[i]);
+            };
+        }
+    }
+
+    var validarFormulario = function(e) {
+
+        var $evento = new Event('checkMe');
+        var $form = $(this).find(':input[data-validator]');
+
+        for (var i = $form.length - 1; i >= 0; i--) {
+            $form[i].dispatchEvent($evento);
+        };
+
+
+        if ($('.red-bg').length > 0) {
+            e.preventDefault();
+        }
+
+    }
 
 })(jQuery);
