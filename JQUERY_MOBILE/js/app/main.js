b55c929e37f6f4916e3dd489a1279f3fd6516d47
0sv <arenedo@gmail.com>
2014-04-09 18:38:35 +0200

    JQuery Mobile

---
+++ JQUERY_MOBILE/js/app/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    "use strict";
+
+    //inicializar la aplicación
+
+    $(document).on('click', '#btnMultiPages', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+    });
+
+
+});
