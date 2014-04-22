10fd47c7fbab207313ca0602adc6a5684034c538
0sv <arenedo@gmail.com>
2014-04-22 18:31:46 +0200

    Twitter webApp Test ajax with spy from SinonJS

--- LECTOR_TWITTER/test/spec/testServices.js
+++ LECTOR_TWITTER/test/spec/testServices.js
@@ -32,6 +32,8 @@
 
         console.log(log);
         console.log(tweets);
+        expect($.ajax.calledOnce).to.be.true;
+        assert.equal('/app/data/tweets.json', $.ajax.firstCall.args[0].url);
         if (tweets && tweets.statuses && tweets.statuses.length === 100) {
             callbackOkGetTweets.done();
         } else {
@@ -40,25 +42,59 @@
 
     }
 
+    var callbackOkAjax = function(log, tweets) {
+        console.log(log);
+        expect($.ajax.calledOnce).to.be.true;
+        assert.equal('/app/data/tweets.json', $.ajax.firstCall.args[0].url);
+        callbackOkAjax.done();
+    }
+
     //
 
     describe('Services module', function() {
         var services;
+        var $;
         beforeEach(function(done) {
-            require(['services'], function(module) {
+
+            require(['services', 'jquery'], function(module, jQuery) {
                 services = module;
+                $ = jQuery;
+                //console.log($);
                 done();
             });
+
         });
 
+
         describe('#Get tweets from cloud', function() {
+            beforeEach(function(done) {
+                sinon.spy($, 'ajax');
+                done();
+            });
+
+            it('$.ajax has been called', function(done) {
+                callbackOkAjax.done = done;
+                services.getTweets("foo_apikey", callbackOkAjax, callbackNok);
+
+
+            })
+
             it('get all tweets', function(done) {
                 callbackOkGetTweets.done = done;
                 services.getTweets("foo_apikey", callbackOkGetTweets, callbackNok);
 
 
             });
+
+            afterEach(function(done) {
+                $.ajax.restore();
+                done();
+            });
+
         });
+
+
+
 
 
     });
